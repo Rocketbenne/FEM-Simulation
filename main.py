ed9819e7ab3e4e84435e92d55ec7e820ed0d504e
Benedikt Unterthurner <benedikt.unterthurner@student.tugraz.at>
2024-05-07 18:41:32 +0200

    branch creation

---
+++ main.py
@@ -0,0 +1,10 @@
+#%%
+import numpy as np
+import time as time
+from scipy.sparse import coo_matrix
+from sklearn.neighbors import BallTree
+import sys
+import matplotlib.pyplot as plt
+
+#def getGeometryInputs():
+#    length, width = 0, 0

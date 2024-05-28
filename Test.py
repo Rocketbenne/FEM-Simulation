462b1acd8bd6d5c1b4ae643f1ab0926599c951bf
Max Tschurtschenthaler <max.tschurtschenthaler@student.tugraz.at>
2024-05-28 16:50:02 +0200

    a better version of the GUI

--- Test.py
+++ Test.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import font
+import MatrixGUI
 
 # Create the main application window
 root = tk.Tk()
@@ -7,37 +8,38 @@ root.title("Simple Tkinter App")
 
 # Create a custom font
 custom_font = font.Font(family="Helvetica", size=32, weight="bold")
+custom_font2 = font.Font(family="Helvetica", size=16, weight="bold")
 
 # Create a label widget
 label1 = tk.Label(root, text="Inputs", font=custom_font)
-label1.grid(row=0, column=1, padx=100, pady=20)
+label1.grid(row=0, column=1)
 
-# Define the number of rows and columns for the grid of entry widgets
-rows = 3
-columns = 3
-
-#Creating Arrays used afterwards to store the data
+# Creating Arrays used afterwards to store the data
 entrys = []
 inputs = []
+lable_names = ["Material Tensor", "Material Tensor", "length", "width", " " , " "]
 
-# Create entry widgets and place them in a grid
-for i in range(1,rows+1):
-    for j in range(columns-1):
-        if(j == 1):
-            entry = tk.Entry(root)
-            entry.grid(row=i, column=j, padx=5, pady=5)
-            entrys.append(entry)
-
+# Create the Grid
+matrix_tensor = MatrixGUI.MatrixGUI(root,2,2,1,1)
+matrix_entry = MatrixGUI.MatrixGUI(root,5,1,3,1)
+matrix_label = MatrixGUI.MatrixGUI(root,5,1,1,0,lable_names)
 # Define a function to update the label with the entry text
 def save_input():
-    for entry in entrys:
-        inputs.append(entry.get())
-    print(inputs)
+    if(matrix_entry.get_matrix().len() > 0):
+        for entry in matrix_entry.get_matrix():
+            inputs.append(entry.get())
+        print(inputs)
+        print_matrix()
+    print("No imputs!")
+
+def print_matrix():
+    matrix = matrix_entry.get_matrix()
+    print(matrix)
 
 
 # Create a button widget that calls the update_label function when clicked
 button = tk.Button(root, text="Save Inputs", command=save_input)
-button.grid(row=6, column=1, padx=5, pady=20)
+button.grid(row=matrix_entry.rows+matrix_entry.start_rows, column=1)
 
 # Start the Tkinter event loop
 root.mainloop()

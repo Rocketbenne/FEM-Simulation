598538baa0213799e410092289249ef3cfb994af
Benedikt Unterthurner <benedikt.unterthurner@student.tugraz.at>
2024-05-31 00:05:32 +0200

    node-equation-array with dict as return value

--- main.py
+++ main.py
@@ -34,8 +34,28 @@ Local Knot Numbers starting from bottom right in a counter-clockwise rotation
     Number in the Middle: Global Element Number
 
 '''
+# Knotengleichungsarray
+def get_node_equation_array(array_size, mesh_coords):
 
+    globale_gleichungsnummer = np.zeros(array_size, dtype=int)
 
+    min_x = np.min(mesh_coords[:, 0])
+    max_x = np.max(mesh_coords[:, 0])
+    min_y = np.min(mesh_coords[:, 1])
+    max_y = np.max(mesh_coords[:, 1])
+
+    i, j = 0, 1  # i for indexing and j for value of globale gleichungsnummer
+    for (x, y) in mesh_coords:
+        #print(x, y)
+        if(x != min_x and x != max_x and y != min_y and y != max_y):
+            globale_gleichungsnummer[i] = j
+            j += 1
+        i += 1
+
+    # converts mesh_coords to tuples, tuples can be indexed using tuple[0] and tuple[1]
+    mesh_coords_tuples = [tuple(coord) for coord in mesh_coords]
+
+    return dict(zip(mesh_coords_tuples, globale_gleichungsnummer))   
 
 
 
@@ -46,3 +66,9 @@ width, height, order_num_int = getGeometryInputs_hard_coded()
 mesh_coords = createMesh(width, height)
 
 visualize_mesh(mesh_coords)
+
+array_size = mesh_coords.shape[0]  # gets amount of coordinate pairs
+
+NE_array = get_node_equation_array(array_size, mesh_coords)
+
+#print(NE_array)
